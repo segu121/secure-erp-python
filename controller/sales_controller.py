a1a3a7d3be276de4a2d642d4c76daeeeb94d3374
segu121 <slowik.sebastian1@gmail.com>
2021-04-20 20:29:44 +0200

    New better world------> Sales four function

--- controller/sales_controller.py
+++ controller/sales_controller.py
@@ -2,64 +2,97 @@ from model.sales import sales
 from view import terminal as view
 
 
+
+
 def list_transactions():
-    list_sale = sales.sale()
-    # view.print_error_message("Not implemented yet.")
-    view.print_table(list_sale)
+    view.print_table(sales.sale())
 
 def add_transaction():
     list_sale = sales.sale()
     view.print_table(list_sale)
 
 
-    label = "Write:  - customer id, product, price, transaction"
-    user_input = view.get_input(label)
-    inputs = view.get_inputs(user_input)
-    sales.create_sale(inputs)
-
+    inputs_value = view.get_inputs(["Customer ID: ", "Product: ", "Price: ", "Transaction Date: "])
+    sales.create_sale(inputs_value)
 
     list_sale = sales.sale()
     view.print_table(list_sale)
 def update_transaction():
     # view.print_error_message("Not implemented yet.")
-    list_sale = sales.sale()
-    view.print_table(list_sale)
-    label = "Write:  Id"
-    user_input = view.get_input(label)
+    view.print_table(sales.sale())
 
-    for element in list_sale:
-        if element[0] == user_input:
-            label1 = "Write: customer id, product, price, transaction"
-            view.get_inputs(label,label1)
-            sales.uptade(label)
-        else:
-            label = "Write:  - customer id, product, price, transaction"
-            user_input = view.get_input(label)
-            inputs = view.get_inputs(user_input)
-            sales.create_sale(inputs)
+    input_id = view.get_input("Transaction ID")
+    inputs_value = view.get_inputs(["Customer ID: ", "Product: ", "Price: ", "Transaction Date: "])
+    sales.uptade(input_id, inputs_value)
+    view.print_table(sales.sale())
 
-    list_sale = sales.sale()
-    view.print_table(list_sale)
 
 def delete_transaction():
-    view.print_error_message("Not implemented yet.")
+    # view.print_error_message("Not implemented yet.")
+    list_sale = sales.sale()
+    view.print_table(list_sale)
+
+    label = "Write: Transaction ID"
+    transaction_id = view.get_input(label)
+
+    sales.delete(transaction_id)
+
+    list_sale = sales.sale()
+    view.print_table(list_sale)
 
 
 def get_biggest_revenue_transaction():
-    view.print_error_message("Not implemented yet.")
+    # view.print_error_message("Not implemented yet.")
+    list_sale = sales.sale()
+    view.print_table(list_sale)
 
+    message = "The transaction with biggest revenue"
+    view.print_message(message)
 
+    max_id, max_produkt = sales.get_biggest_product_revenue()
+    view.print_message(max_id)
 def get_biggest_revenue_product():
-    view.print_error_message("Not implemented yet.")
+    # view.print_error_message("Not implemented yet.")
+    list_sale = sales.sale()
+    view.print_table(list_sale)
 
+    message = "The transaction with biggest revenue"
+    view.print_message(message)
+
+    max_id, max_produkt = sales.get_biggest_product_revenue()
+    view.print_message(max_produkt)
 
 def count_transactions_between():
-    view.print_error_message("Not implemented yet.")
+    # view.print_error_message("Not implemented yet.")
+    list_sale = sales.sale()
+    view.print_table(list_sale)
 
+    message = "To count transaction write date"
+    view.print_message(message)
 
+    label = "Write: Date first transaction like Year, Month, Day"
+    date_from = view.get_input(label)
+    label1 = "Write: Date second transaction like Year, Month, Day"
+    date_to = view.get_input(label1)
+    number_tranaction = sales.number_of_transactions(date_from, date_to)
+
+    view.print_message(number_tranaction)
 def sum_transactions_between():
-    view.print_error_message("Not implemented yet.")
+    # view.print_error_message("Not implemented yet.")
 
+    list_sale = sales.sale()
+    view.print_table(list_sale)
+
+    message = "To count transaction write date"
+    view.print_message(message)
+
+    label = "Write: Date first transaction like Year, Month, Day"
+    date_from = view.get_input(label)
+    label1 = "Write: Date second transaction like Year, Month, Day"
+    date_to = view.get_input(label1)
+    sum_transaction = sales.sum_transaction(date_from, date_to)
+
+    view.print_message(sum_transaction)
 
 def run_operation(option):
     if option == 1:
@@ -85,17 +118,16 @@ def run_operation(option):
 
 
 def display_menu():
-    options = ["1 ---> List transactions",
-               "2 ---> Add new transaction",
-               "3 ---> Update transaction",
-               "4 ---> Remove transaction",
-               "5 ---> Get the transaction that made the biggest revenue",
-               "6 ---> Get the product that made the biggest revenue altogether",
-               "7 ---> Count number of transactions between",
-               "8 ---> Sum the price of transactions between",
-               "0 ---> Back to main menu"]
-    view.print_menu("   ***ERP***\n     Sales:", options)
- 
+    options = ["Back to main menu",
+               "1 --> List transactions",
+               "2 --> Add new transaction",
+               "3 --> Update transaction",
+               "4 --> Remove transaction",
+               "5 --> Get the transaction that made the biggest revenue",
+               "6 --> Get the product that made the biggest revenue altogether",
+               "7 --> Count number of transactions between",
+               "8 --> Sum the price of transactions between"]
+    view.print_menu("Sales", options)
 
 
 def menu():
@@ -103,7 +135,7 @@ def menu():
     while operation != '0':
         display_menu()
         try:
-            operation = view.get_input("Select an operation: ")
+            operation = view.get_input("Select an operation")
             run_operation(int(operation))
         except KeyError as err:
             view.print_error_message(err)
--- model/sales/sales.py
+++ model/sales/sales.py
@@ -7,41 +7,68 @@ Data table structure:
     - price (float)
     - transaction date (string): in ISO 8601 format (like 1989-03-21)
 """
-
+import datetime
 from model import data_manager, util
+
 
 DATAFILE = "model/sales/sales.csv"
 HEADERS = ["Id", "Customer", "Product", "Price", "Date"]
 
-LIST_SALES = data_manager.read_table_from_file(DATAFILE)
-new_id = util.generate_id
 
 
-
-
+def new_id():
+    tr_id = util.generate_id()
+    return tr_id
 def sale():
-    list_sale = data_manager.read_table_from_file(DATAFILE)
-    return list_sale
-def create_sale(sale):
-    list_sale = sale()
-    sale.insert(0, util.generate_id())
-    list_sale.append(sale)
-
-    data_manager.write_table_to_file(DATAFILE, list_sale)
+    sales = data_manager.read_table_from_file(DATAFILE)
+    table = [HEADERS] + sales
+    return table
 
 
-def uptade(input_id, input_customer, input_product, price, transaction_date):
-    list_sale = sale()
-    for element in list_sale:
-        if element[0] == input_id:
-            element[1] = input_customer
-            element[2] = input_product
-            element[3] = price
-            element[4] = transaction_date
+def list_sale():
+    sales = data_manager.read_table_from_file(DATAFILE)
+    return sales
 
-            list_sale.append(element)
 
-    data_manager.write_table_to_file(DATAFILE, list_sale)
+
+
+
+def create_sale(inputs_value):
+    sales = list_sale()
+    tr_id = new_id()
+    random_id = "".join(tr_id)
+    print(random_id)
+    new_sale = [[random_id]]
+    for element in inputs_value:
+
+        new_sale[0].extend([element])
+
+    sales.extend(new_sale)
+
+    data_manager.write_table_to_file(DATAFILE, sales)
+
+
+
+def uptade(input_id, inputs_value):
+    sales = list_sale()
+    separator = ";"
+    for row in range(len(sales)):
+        tr_id = "".join(sales[row][0])
+        if tr_id == (input_id):
+
+            sales[row][1] = inputs_value[0]
+            sales[row][2] = inputs_value[1]
+            sales[row][3] = inputs_value[2]
+            sales[row][4] = inputs_value[3]
+            print(sales)
+            if sales[row][1] == inputs_value[0] and sales[row][4] == inputs_value[3]:
+                # data_manager.read_table_from_file(DATAFILE, sales)
+                with open(DATAFILE, "w") as file:
+                    for record in sales:
+                        row = separator.join(record)
+                        file.write(row + "\n")
+
+
 
 def delete(input_id):
     list_sale = sale()
@@ -49,7 +76,7 @@ def delete(input_id):
         if element[0] == input_id:
             list_sale.remove(element)
 
-    data_manager.write_table_to_file(DATAFILE, list_sale)
+        data_manager.write_table_to_file(DATAFILE, list_sale)
 
 
 def get_biggest_product_revenue():
@@ -58,18 +85,24 @@ def get_biggest_product_revenue():
     try:
         for element in list_sale:
             price.append(element[3])
+            res_max = max(price, key=lambda x: float(x))
+            for element in list_sale:
+                try:
+                    if element[3] == res_max:
+                        max_id = element[0]
+                        max_produkt = element[2]
+                        return max_id, max_produkt
     except IndexError:
         print()
-    res_max = max(price, key=lambda x: float(x))
-    for element in list_sale:
-        try:
-            if element[3] == res_max:
-                max_id = element[0]
-                max_produkt = element[2]
-                print(max_id)
-                print(max_produkt)
-        except IndexError:
-            print()
+    # res_max = max(price, key=lambda x: float(x))
+    # for element in list_sale:
+    #     try:
+    #         if element[3] == res_max:
+    #             max_id = element[0]
+    #             max_produkt = element[2]
+    #             return max_id, max_produkt
+    #     except IndexError:
+    #         pass
 
 
 
@@ -90,7 +123,7 @@ def number_of_transactions(input_date_1, input_date_2):
         if (input_date_1 <= list_of_date[x] and input_date_2 >= list_of_date[x]):
             list_transaction.append(list_of_date[x])
     number_tranaction = len(list_transaction)
-    return number_tranaction
+    return str(number_tranaction)
 
 
 
@@ -117,108 +150,6 @@ def sum_transaction(input_date_1, input_date_2):
 
     print(list_price)
     sum_transaction = sum(map(float, list_price))
-    return sum_transaction
-(LIST_SALES)
+    return str(sum_transaction)
 
 
-
-
-def sale():
-    list_sale = data_manager.read_table_from_file(DATAFILE)
-    return list_sale
-def create_sale(sale):
-    list_sale = sale()
-    sale.insert(0, util.generate_id())
-    list_sale.append(sale)
-
-    data_manager.write_table_to_file(DATAFILE, list_sale)
-
-
-def uptade(input_id, input_customer, input_product, price, transaction_date):
-    list_sale = sale()
-    for element in list_sale:
-        if element[0] == input_id:
-            element[1] = input_customer
-            element[2] = input_product
-            element[3] = price
-            element[4] = transaction_date
-
-            list_sale.append(element)
-
-    data_manager.write_table_to_file(DATAFILE, list_sale)
-
-def delete(input_id):
-    list_sale = sale()
-    for element in list_sale:
-        if element[0] == input_id:
-            list_sale.remove(element)
-
-    data_manager.write_table_to_file(DATAFILE, list_sale)
-
-
-def get_biggest_product_revenue():
-    list_sale = sale()
-    price = []
-    try:
-        for element in list_sale:
-            price.append(element[3])
-    except IndexError:
-        print()
-    res_max = max(price, key=lambda x: float(x))
-    for element in list_sale:
-        try:
-            if element[3] == res_max:
-                max_id = element[0]
-                max_produkt = element[2]
-                print(max_id)
-                print(max_produkt)
-        except IndexError:
-            print()
-
-
-
-def number_of_transactions(input_date_1, input_date_2):
-
-
-    list_sale = sale()
-    list_transaction = []
-    list_of_date = []
-    try:
-        for element  in list_sale:
-            list_of_date.append(element[4])
-    except IndexError:
-        print()
-    # for x in list_of_date:
-    #     if input_date_1 <= x and x >= input_date_2:
-    for x in range(len(list_of_date)):
-        if (input_date_1 <= list_of_date[x] and input_date_2 >= list_of_date[x]):
-            list_transaction.append(list_of_date[x])
-    number_tranaction = len(list_transaction)
-    return number_tranaction
-
-
-
-
-def sum_transaction(input_date_1, input_date_2):
-    list_sale = sale()
-    list_price = []
-    list_of_date = []
-    try:
-        for element  in list_sale:
-            list_of_date.append(element[4])
-
-    except IndexError:
-        print()
-    print(list_of_date)
-    k = len(list_of_date) - 1
-    try:
-        for x in range(len(list_of_date)):
-            if input_date_1 <= list_of_date[x] and input_date_2 >= list_of_date[x]:
-                list_price.append(list_sale[x][3])
-
-    except IndexError:
-        print()
-
-    print(list_price)
-    sum_transaction = sum(map(float, list_price))
-    return sum_transaction
